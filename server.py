6630d1373d81f7b06cff4c8dadffb9be5f9d84bd
Natur <a.rivas06@ufromail.cl>
2024-03-31 12:54:21 -0300

    Añade la rama server

--- server.py
+++ server.py
@@ -3,21 +3,21 @@ app = Flask(__name__)
 
 PORT = 8081
 
-AR_tareas = []
+tareas = []
 
 
 # Devuelve la lista de tareas
 @app.route("/tareas", methods=["GET"])
 def get_tareas():
-    return jsonify(AR_tareas), 200
+    return jsonify(tareas), 200
 
 
 # Ingresa una nueva tarea a la lista
 @app.route("/tareas", methods=["POST"])
 def post_tarea():
-    AR_nueva_tarea = request.json
-    AR_tareas.append(AR_nueva_tarea)
-    return jsonify(AR_nueva_tarea), 200
+    nueva_tarea = request.json
+    tareas.append(nueva_tarea)
+    return jsonify(nueva_tarea), 200
 
 
 # Actualiza los datos de una tarea, en caso de no proveer información 
@@ -25,12 +25,12 @@ def post_tarea():
 @app.route("/tareas", methods=["PUT"])
 def put_tarea():
 
-    AR_nombre_tarea = request.json["nombre"]
-    AR_status_tarea = request.json["hecha"]
+    nombre = request.json["nombre"]
+    hecha = request.json["hecha"]
 
-    for AR_tarea in AR_tareas:
-        if AR_tarea["nombre"] == AR_nombre_tarea:
-            AR_tarea["hecha"] = "si"
+    for tarea in tareas:
+        if tarea["nombre"] == nombre:
+            tarea["hecha"] = hecha
             return jsonify(
                 {"message": "Tarea actualizada"}), 200
             
@@ -41,11 +41,11 @@ def put_tarea():
 # Elimina una tarea
 @app.route("/tareas", methods=["DELETE"])
 def delete_tarea():
-    AR_nombre_tarea = request.json["nombre"]
+    nombre = request.json["nombre"]
 
-    for AR_tarea in AR_tareas:
-        if AR_tarea["nombre"] == AR_nombre_tarea:
-            AR_tareas.remove(AR_tarea)
+    for tarea in tareas:
+        if tarea["nombre"] == nombre:
+            tareas.remove(tarea)
             return jsonify({"message": "Tarea eliminada"}), 200
 
     return jsonify({"message": "Tarea no encontrada"}), 404
